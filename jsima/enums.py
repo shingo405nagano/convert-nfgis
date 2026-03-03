1924088f67ae5caaff1d02715b9da3c8082819a6
shingo405nagano <makishin.nagano@gmail.com>
2026-03-03 07:57:35 +0000

    [Add] 地目とポリゴン回転の列挙型を追加しJPGIS定義を拡張

--- jsima/enums.py
+++ jsima/enums.py
@@ -1,7 +1,8 @@
-from enum import StrEnum, Enum # noqa: F401
+from enum import Enum, StrEnum  # noqa: F401
+
 
 class JsimaCoordinateSystemEnum(Enum):
-    """ JPGISの座標系を表す列挙型
+    """JPGISの座標系を表す列挙型
 
     ここでは平面直角座標系の系番号のみを定義する。
     平面直角座標系は日本全国を19の系に分割している。
@@ -28,6 +29,7 @@ class JsimaCoordinateSystemEnum(Enum):
      - https://www.gsi.go.jp/sokuchikijun/jpc_18.html
      - https://www.gsi.go.jp/sokuchikijun/jpc_19.html
     """
+
     JPR_01 = 1
     JPR_02 = 2
     JPR_03 = 3
@@ -50,19 +52,56 @@ class JsimaCoordinateSystemEnum(Enum):
 
 
 class JsimaCrsEnum(Enum):
-    """ JPGISの測地系を表す列挙型
+    """JPGISの測地系を表す列挙型
 
     使用するタグは'<jsima:Crs>'
     """
+
     JGD_2000 = 1
     JGD_2011 = 2
     JGD_2024 = 3
+
 
 class JsimaJpsUuidRefEnum(StrEnum):
     """JPGISのCRS UUIDを表す列挙型
 
     使用するタグは'<jps:CRS uuidref="..."/>'
     """
+
     JGD_2000_PL10 = "jpsuuid:jgd2000.pl10"
     JGD_2011_PL10 = "jpsuuid:jgd2011.pl10"
     JGD_2024_PL10 = "jpsuuid:jgd2024.pl10"
+
+
+class JsimaChimokuEnum(Enum):
+    """JPGISの地物を表す列挙型
+
+    使用するタグは'<jsima:Chimoku>'
+     - 田んぼ: 1
+     - 畑: 2
+     - 池沼: 6
+     - 山林: 7
+     - 牧場: 8
+     - 保安林: 18
+     - その他: 24
+    """
+
+    TANBO = 1
+    HATAKE = 2
+    CHINUMA = 6
+    SANRIN = 7
+    BOKUJO = 8
+    HOANRIN = 18
+    SONOTA = 24
+
+
+class JsimaPolyRotationEnum(Enum):
+    """JPGISのポリゴンの回転方向を表す列挙型
+
+    使用するタグは'<jsima:PolyRotation>'
+     - 時計回り: 0
+     - 反時計回り: 1
+    """
+
+    CLOCKWISE = 0
+    COUNTERCLOCKWISE = 1
