7752d7fd33f28c716222901ea4aca49212c7cd36
shingo405nagano <makishin.nagano@gmail.com>
2026-03-05 07:28:11 +0000

    [Update] requirements

--- nfgis/libs/__init__.py
+++ nfgis/libs/__init__.py
@@ -6,11 +6,16 @@ from .sima.jsima.enums import (
     JsimaPolyRotationEnum,
 )
 from .sima.jsima.gm_point import JsimaGmPointModel, JsimaGmPointModels
-from .sima.jsima.gm_polygon import JsimaGmPolygonModel, dissambly_poly_to_dict
+from .sima.jsima.gm_polygon import (
+    JsimaGmPolygonModel,
+    dissambly_poly_to_dict,
+    replacement_xy,
+)
 from .sima.jsima.xml_builder import JsimaXmlBuilder
 
 __all__ = [
     "dissambly_poly_to_dict",
+    "replacement_xy",
     "JsimaGmPointModel",
     "JsimaGmPointModels",
     "JsimaGmPolygonModel",
